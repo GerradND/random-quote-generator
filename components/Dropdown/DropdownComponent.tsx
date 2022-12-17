152e0549255189574f64d6ae47505eefb28b23c9
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-12-17 18:11:20 +0700

    Add props

--- components/Dropdown/DropdownComponent.tsx
+++ components/Dropdown/DropdownComponent.tsx
@@ -1,18 +1,28 @@
-export default function DropdownComponent({num, setNum}) {
-    const choices = Array.from(Array(6).keys()).map(x => x + 1);
+import React, { ChangeEvent, Dispatch, SetStateAction } from 'react';
 
-    const handleChange = (event) => {
-        setNum(event.target.value)
-    }
+interface DropdownProps {
+	num: number;
+	setNum: Dispatch<SetStateAction<number>>;
+}
 
-    return (
-        <div>
-            <select className="inline-flex w-full justify-center rounded-md border-2 border-black bg-white px-2 py-2 my-2 text-gray-700 shadow-sm hover:bg-gray-50"
-                value={num} onChange={handleChange}>
-                {choices.map((value) => (
-                    <option key={value}>{value}</option>
-                ))}
-            </select>
-        </div>
-    );
+export default function DropdownComponent({ num, setNum }: DropdownProps) {
+	const choices = Array.from(Array(6).keys()).map((x) => x + 1);
+
+	const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+		setNum(parseInt(event.target.value));
+	};
+
+	return (
+		<div>
+			<select
+				className="inline-flex w-full justify-center rounded-md border-2 border-black bg-white px-2 py-2 my-2 text-gray-700 shadow-sm hover:bg-gray-50"
+				value={num}
+				onChange={handleChange}
+			>
+				{choices.map((value) => (
+					<option key={value}>{value}</option>
+				))}
+			</select>
+		</div>
+	);
 }
