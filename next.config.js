01d457e8d541cc90b3fbbdd34f8ef8beb5a52f9e
Mario Serano <marioserano55@gmail.com>
2022-12-18 15:44:52 +0700

    chore: try with runtime config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-}
+  output: "standalone",
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
